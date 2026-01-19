828caeaa77498db20ecfb24afb297536084565eb
Riyadulislamratul <riadul1810833319@gmail.com>
2026-01-19 23:07:43 +0600

    Models added

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Students(models.Model):
+    student_ID = models.models.IntegerField(_)
+    full_name = models.TextField(max_length=100)
+    department = models.TextField(max_length=100)
+    gender = models.TextField(max_length=10)
+    address = models.TextField(max_length=200)
+    date_of_birth = models.DateField()
+
+    def __str__(self):
+        return f"{self.student_ID} - {self.full_name}"
